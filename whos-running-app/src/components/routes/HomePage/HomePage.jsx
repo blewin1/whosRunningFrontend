83d29b5da9e8aeb81c27fc44abc7b48195223400
Gaspy <mpgasbarro@gmail.com>
2020-10-01 17:40:06 -0400

    styled front page

--- whos-running-app/src/components/routes/HomePage/HomePage.jsx
+++ whos-running-app/src/components/routes/HomePage/HomePage.jsx
@@ -1,22 +1,29 @@
 import React from 'react';
 import AddressForm from '../../AddressForm/AddressForm';
 import Flex from '../../layout/Flex';
+import HeaderNav from "../../HeaderNav";
+import HomePageStyled from './HomePage.styled';
 
 const HomePage = ({ history }) => {
 	return (
+		<div> 
+		<HomePageStyled> 
+		<HeaderNav />
 		<Flex
 			alignItems='center'
 			justifyContent='start'
 			direction='column'
 			height='100vh'
 			padding='0 35px'>
-			<h2>Be an informed voter.</h2>
-			<p>
+			<h2 className="informed">Be an informed voter.</h2>
+			<p className = "top">
 				View the elections you will be voting in and learn more about
 				the candidates by entering your address.
 			</p>
 			<AddressForm history={history} />
 		</Flex>
+		</HomePageStyled>
+		</div>
 	);
 };
 
---
+++ whos-running-app/src/components/routes/HomePage/HomePage.styled.jsx
@@ -0,0 +1,38 @@
+import styled from "styled-components";
+
+export default styled.div`
+	.informed {
+		width: 295px;
+		height: 30px;
+		font-family: Merriweather;
+		font-style: normal;
+		font-weight: bold;
+		font-size: 24px;
+		line-height: 30px;
+		color: #ff7676;
+	}
+	.welcome {
+		position: absolute;
+		width: 61px;
+		height: 32px;
+		left: 300px;
+		top: 27px;
+		font-family: Noto Sans;
+		font-style: normal;
+		font-weight: normal;
+		font-size: 12px;
+		line-height: 16px;
+        text-align: center;
+        color: white;
+	}
+	.top {
+		width: 359px;
+		height: 70px;
+		font-family: Noto Sans;
+		font-style: normal;
+		font-weight: bold;
+		font-size: 16px;
+		line-height: 150%;
+		color: #29395f;
+	}
+`;
