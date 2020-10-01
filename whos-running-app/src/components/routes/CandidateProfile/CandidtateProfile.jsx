12642083e1b1210da233a4abada55de912e0a230
Gaspy <mpgasbarro@gmail.com>
2020-10-01 11:43:37 -0400

    add functionality of Candidate Profile

--- whos-running-app/src/components/routes/CandidateProfile/CandidtateProfile.jsx
+++ whos-running-app/src/components/routes/CandidateProfile/CandidtateProfile.jsx
@@ -1,11 +1,65 @@
-import React from 'react'
-import HeaderNav from '../../HeaderNav'
+import React, { useEffect, useState } from 'react';
+import Nav from '../../Nav';
+import HeaderNav from '../../HeaderNav';
+import { getCandidates } from '../../../utils/apiUtils';
+import { positions } from '../BallotPage/BallotPositionsList';
+import CandidatesContainer from '../../Styled/CandidatesContainer';
+import CandidateOption from '../../CandidateOption/CandidateOption';
+import { Route, Link } from 'react-router-dom';
+import BallotPage from "../BallotPage/BallotPage"
+
 
 const CandidatePage = (props) => {
+    const [candidates, setCandidates] = useState([]);
+	const [selectedPosition, setSelectedPostion] = useState(0);
+
+	useEffect(() => {
+		const initialize = async () => {
+			setCandidates(await getCandidates());
+		};
+		initialize();
+	}, []);
+
+    let candidateInfo = candidates.map((candi, index) => {
+		if (props.match.params.candidateId == candi.id) {
+			return (
+				<div>
+					<img src={candi.image_url} alt='pic' />
+					<em>{candi.bio}</em>
+					<h1>{candi.name} </h1>
+					<h3>{candi.position} </h3>
+					<p>{candi.bio}</p>
+					<a href={candi.twitter}> Twitter </a>
+					<a href={candi.facebook}> Facebook </a>
+					<a href={candi.campaign_website}> Campaign website </a>
+				</div>
+			);
+		}
+    });
+    let notCandidateInfo = candidates.map((candi, index) => {
+		if (props.match.params.candidateId !== candi.id) {
+			return (
+				<div>
+					<img src={candi.image_url} alt='pic' />
+					<em>{candi.bio}</em>
+					<h1>{candi.name} </h1>
+					<h3>{candi.position} </h3>
+					<p>{candi.bio}</p>
+					<a href={candi.twitter}> Twitter </a>
+					<a href={candi.facebook}> Facebook </a>
+					<a href={candi.campaign_website}> Campaign website </a>
+				</div>
+			);
+		}
+    });
+    console.log(candidates)
     return (
         <div>
             <HeaderNav />
-            Candidate Page 
+            <nav> 
+            <Link to='/ballot'> All Candidates </Link>
+            </nav>
+            {candidateInfo}
         </div>
     )
 }
