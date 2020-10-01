1c7c4b79b5ab0e1022d36a02343c72b771cbb829
Gaspy <mpgasbarro@gmail.com>
2020-10-01 15:24:18 -0400

    added styling to the candidate profile page

---
+++ whos-running-app/src/components/routes/CandidateProfile/CandidatePage.styled.jsx
@@ -0,0 +1,151 @@
+import styled from 'styled-components';
+
+export default styled.div`
+	.twitter {
+		position: absolute;
+		width: 32px;
+		height: 32px;
+		left: 277px;
+		top: 576px;
+	}
+	.facebook {
+		position: absolute;
+		width: 24px;
+		height: 24px;
+		left: 320px;
+		top: 580px;
+	}
+	.links {
+		position: absolute;
+		width: 28px;
+		height: 28px;
+		left: 354px;
+		top: 577px;
+	}
+	.socialLinks {
+		float: right;
+	}
+	// .picAndBio{
+	//     display: flex;
+	//     justify-content: space-around;
+	//     padding-left: 20px;
+	// 	padding-top: 100px;
+	// }
+	.bigPic {
+		border-radius: 100%;
+		border: 4px solid black;
+		height: 125px;
+		width: 125px;
+		background-size: cover; //contain
+		background-repeat: no-repeat;
+		background-position: start center;
+		position: absolute;
+		width: 160px;
+		height: 160px;
+		left: 20px;
+		top: 135px;
+	}
+	.bioOfCandi {
+		position: absolute;
+		width: 196px;
+		height: 133px;
+		left: 198px;
+		top: 131px;
+		font-family: Noto Sans;
+		font-style: italic;
+		font-weight: normal;
+		font-size: 14px;
+		line-height: 19px;
+		color: #727e9a;
+	}
+	.candiName {
+		position: absolute;
+		width: 172px;
+		height: 20px;
+		left: 20px;
+		top: 323px;
+		font-family: Merriweather;
+		font-style: normal;
+		font-weight: bold;
+		font-size: 18px;
+		line-height: 23px;
+
+		color: #ff7676;
+	}
+	.candiPosition {
+		position: absolute;
+		width: 376px;
+		height: 36px;
+		left: 20px;
+		top: 357px;
+		font-family: Merriweather;
+		font-style: normal;
+		font-weight: bold;
+		font-size: 14px;
+		line-height: 18px;
+		color: #29395f;
+	}
+	.candiBio {
+		position: absolute;
+		width: 374px;
+		height: 162px;
+		left: 20px;
+		top: 404px;
+		font-family: Noto Sans;
+		font-style: normal;
+		font-weight: 500;
+		font-size: 12px;
+		line-height: 150%;
+		color: #29395f;
+	}
+	.secondCandi {
+		border-radius: 100%;
+		border: 4px solid black;
+		height: 125px;
+		width: 125px;
+		background-size: cover; //contain
+		background-repeat: no-repeat;
+		background-position: start center;
+		position: absolute;
+		width: 98.67px;
+		height: 148px;
+		left: 61px;
+		top: 692px;
+	}
+	.secondName {
+		position: absolute;
+		width: 177px;
+		height: 39px;
+		left: 21px;
+		top: 820px;
+		font-family: Noto Sans;
+		font-style: normal;
+		font-weight: bold;
+		font-size: 14px;
+		line-height: 150%;
+		text-align: center;
+		color: #1fbcde;
+	}
+	.line {
+		position: absolute;
+		width: 359px;
+		height: 0px;
+		left: 28px;
+		top: 623px;
+		border: 1px solid #e8eced;
+	}
+	.whatPos {
+		position: absolute;
+		width: 376px;
+		height: 54px;
+		left: 20px;
+		top: 642px;
+		font-family: Merriweather;
+		font-style: normal;
+		font-weight: bold;
+		font-size: 14px;
+		line-height: 18px;
+
+		color: #29395f;
+	}
+`;
